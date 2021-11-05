119112cfcf8651fcc509b3d084c418e42edaee87
gabrielamurillounah <ggmurillo@unah.hn>
2021-11-05 00:10:34 -0600

    Primer commit del Proyecto G8_19

---
+++ Config/Conexion.php
@@ -0,0 +1,20 @@
+<?php
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+          try {
+             $conectar = $this->dbh = new PDO("mysql:host=34.68.196.220;dbname=g8_19","G8_19","Pn4tiUyt");
+             return $conectar; 
+            } catch (Exection $e) {
+             print "Error BD: " . $e->getMessage() . "<br/>";
+             die();
+            }
+
+        }
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+    }
+
+?>
---
+++ Controller/pedidos.php
@@ -0,0 +1,41 @@
+<?php
+    header('Content-Type: aplication/json');
+
+    require_once("../config/conexion.php");
+    require_once("../Models/Pedidos.php");
+    $pedidos = new Pedidos();
+
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    switch($_GET["op"]){
+
+            case"GetPedidos":
+                $datos=$pedidos->get_pedidos();
+                echo json_encode($datos);
+            break;
+
+            case"GetUno":
+                $datos=$pedidos->get_pedido($body["ID"]);
+                echo json_encode($datos);
+            break;
+
+            case"InsertPedido":
+                $datos=$pedidos->insert_pedido($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],
+                $body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"]);
+                echo json_encode("Pedidos Agregados");
+            break;
+
+            case "EliminarPedidos":
+                $datos=$pedidos->delete_pedido($body["ID"]);
+                echo json_encode("Pedidos Eliminados");
+            break;
+        
+            case "ActualizarPedido":
+                $datos=$pedidos->actualizar_pedido($body["ID"],$body["ID_SOCIO"],$body["FECHA_PEDIDO"],
+                $body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_ENTREGA"],$body["ESTADO"]);
+                echo json_encode("Pedidos Actualizados");
+            break;
+        
+        
+    }
+?>
